bc9a109f0464f84caba2f69a6a4e05d9a3b9337b
Mansur Akhmatov <ahmatowmansur@gmail.com>
2022-10-14 19:08:56 +0300

    removed masstransit

--- Back/Program.cs
+++ Back/Program.cs
@@ -1,12 +1,9 @@
-using System.Reflection;
 using Back;
 using Back.Hubs;
-using Back.Cucumbers;
 using Back.RabbitMQ.Consumer;
 using Back.RabbitMQ.Producer;
 using Back.Repositories;
 using Back.Services;
-using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,22 +32,6 @@ services.AddCors(options =>
             .WithOrigins("http://localhost", "http://192.168.76.216")
             .AllowCredentials();
     });
-});
-
-services.AddMassTransit(config =>
-{
-    config.SetKebabCaseEndpointNameFormatter();
-    config.SetInMemorySagaRepositoryProvider();
-
-    config.AddCucumber<DefaultCucumber>();
-
-    var assembly = Assembly.GetEntryAssembly();
-
-    config.AddSagaStateMachines(assembly);
-    config.AddSagas(assembly);
-    config.AddActivities(assembly);
-
-    config.UsingInMemory((context, cfg) => cfg.ConfigureEndpoints(context));
 });
 
 var app = builder.Build();
--- Back/Services/MessagesService.cs
+++ Back/Services/MessagesService.cs
@@ -1,21 +1,18 @@
 using Back.Entities;
 using Back.RabbitMQ.Producer;
 using Back.Repositories;
-using MassTransit;
 
 namespace Back.Services;
 
 public class MessagesService
 {
     private readonly MessagesRepository _messagesRepository;
-    private readonly IPublishEndpoint _publishEndpoint;
     private readonly IMessageProducer _messagePublisher;
 
-    public MessagesService(MessagesRepository messagesRepository, IPublishEndpoint publishEndpoint,
+    public MessagesService(MessagesRepository messagesRepository,
         IMessageProducer messagePublisher)
     {
         _messagesRepository = messagesRepository;
-        _publishEndpoint = publishEndpoint;
         _messagePublisher = messagePublisher;
     }
 
@@ -24,8 +21,6 @@ public class MessagesService
     public async Task<Message> AddMessage(Message message)
     {
         _messagePublisher.SendMessage(message);
-
-        //await _publishEndpoint.Publish(message);
         return message;
     }
 
