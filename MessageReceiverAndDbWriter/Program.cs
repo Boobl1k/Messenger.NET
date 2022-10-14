e7a431df62944fda6869b437d9f7c3f2386f5b02
Mansur Akhmatov <ahmatowmansur@gmail.com>
2022-10-14 22:45:42 +0300

    message writer improvements

--- MessageReceiverAndDbWriter/Program.cs
+++ MessageReceiverAndDbWriter/Program.cs
@@ -1,39 +1,53 @@
 ﻿using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using MessageReceiverAndDbWriter;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-var factory = new ConnectionFactory { HostName = "rabbitmq" };
-var connection = factory.CreateConnection();
-var channel = connection.CreateModel();
-channel.QueueDeclare(queue: "messages", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-Console.WriteLine("Connection opened");
-
-var context = new AppDbContext();
-
-
-await Task.Run(async () =>
+for (;;)
 {
-    var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (ch, ea) =>
+    IConnection? connection = null;
+    try
     {
-        var content = Encoding.UTF8.GetString(ea.Body.ToArray());
-        var message = JsonSerializer.Deserialize<Message>(content);
-        Console.WriteLine(content);
-        context.Messages.Add(message);
-        context.SaveChanges();
-        channel.BasicAck(ea.DeliveryTag, false);
-    };
-    for(;;)
-    {
-        channel.BasicConsume("messages", false, consumer);
-        await Task.Delay(10);
+        var factory = new ConnectionFactory { HostName = "rabbitmq" };
+        connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "messages", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+        Console.WriteLine("Connection opened");
+
+        var context = new AppDbContext();
+
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (ch, ea) =>
+        {
+            var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+            var message = JsonSerializer.Deserialize<Message>(content);
+            if (message is null)
+            {
+                Console.WriteLine($"Broken message: '{content}'");
+                return;
+            }
+
+            Console.WriteLine($"New message: {message.UserName} says '{message.Text}'");
+            context.Messages.Add(message);
+            context.SaveChanges();
+            channel.BasicAck(ea.DeliveryTag, false);
+        };
+        for (;;)
+        {
+            channel.BasicConsume("messages", false, consumer);
+        }
     }
-});
-
-Console.WriteLine("Closing connection");
-
-connection.Close();
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Closing connection");
+        connection?.Close();
+        await Task.Delay(100);
+    }
+}
