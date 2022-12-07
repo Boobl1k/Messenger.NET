832273c273d8c0fba6b443b37396f184f95dde4b
mansur <ahmatowmansur@gmail.com>
2022-12-07 19:29:47 +0300

    copied mybook projects

---
+++ MyBook/MyBook/wwwroot/js/site.js
@@ -0,0 +1,37 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function AddDeleteFavorites(actionUrl)
+{
+    $(document).ready(function() {
+        $.ajax({
+            type: "POST",
+            url: actionUrl,
+            dataType: "json",
+            contentType: 'application/json; charset=utf-8',
+            success: function(data) {
+                $('#modalText').text(data.msg);
+            },
+            error: function() {
+                $('#modalText').text("Ошибка");
+            }
+        });
+    });
+}
+
+$(function() {
+    $('#inputGroupSelect01').change(function() {
+        sessionStorage.setItem('searchSelect', this.value);
+    });
+    if(sessionStorage.getItem('searchSelect')){
+        $('#inputGroupSelect01').val(sessionStorage.getItem('searchSelect'));
+    }
+
+    $('#searchKeyword').change(function() {
+        sessionStorage.setItem('searchKeyword', this.value);
+    });
+    if(sessionStorage.getItem('searchKeyword')){
+        $('#searchKeyword').val(sessionStorage.getItem('searchKeyword'));
+    }
+});
